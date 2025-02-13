22cee6865baa69aeabc470960c219388f13c2f5a
Pedro Augusto <hpedroaugusto@hotmail.com>
2025-02-13 14:45:13 -0300

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const updatedArray = new Array();
+    for (let item of array) {
+        if (!args.includes(item)) {
+            updatedArray.push(item);
+        }
+    }
+
+    return updatedArray;
 };
 
 // Do not edit below this line

efd37c76cb21d706e8a868539ca2845907029e35
Pedro Augusto <hpedroaugusto@hotmail.com>
2025-02-14 13:31:07 -0300

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let lesserValue;
+    let greaterValue;
 
+    if (num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number" 
+        || !Number.isInteger(num1) || !Number.isInteger(num2)) 
+    {
+        return "ERROR"
+    }
+
+    if (num1 > num2) {
+        greaterValue = num1;
+        lesserValue = num2;
+    } else {
+        greaterValue = num2;
+        lesserValue = num1;
+    }
+
+    for (let i = lesserValue; i <= greaterValue; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
